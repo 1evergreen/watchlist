654b061ef0b852223a2e8850e5bf07e173cde3e6
1evergreen <44565285+1evergreen@users.noreply.github.com>
2023-11-07 14:29:22 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -18,15 +18,23 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
     return f'hello, {name}'
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+
+# 模板上下文处理函数
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -70,3 +78,4 @@ def forge():
     
     db.session.commit()
     click.echo('Done.')
+    
