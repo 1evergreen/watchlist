dcd068d30620d44823fe50cbb3efaf84e57071a7
1evergreen <44565285+1evergreen@users.noreply.github.com>
2023-11-07 15:03:08 +0800

    Create , edit and delete item by form"

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import click
 import os 
@@ -11,13 +11,25 @@ prefix = 'sqlite:///' if WIN else 'sqlite:////'
 # Note: 在扩展类实例化之前加载配置
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid Input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
@@ -29,12 +41,36 @@ def user_page(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 # 模板上下文处理函数
 @app.context_processor
 def inject_user():
     user = User.query.first()
     return dict(user=user)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid Input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item Updated')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST', 'GET'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted')
+    return redirect(url_for('index'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
